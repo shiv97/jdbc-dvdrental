033879f427355e9c3211b00e14e2cab59312690d
shiv97 <35682403+shiv97@users.noreply.github.com>
2019-08-14 16:13:41 +0530

    Add files via upload

--- dvdrental_java/example/src/main/java/com/example/start/Application.java
+++ dvdrental_java/example/src/main/java/com/example/start/Application.java
@@ -1,13 +1,25 @@
 package com.example.start;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 class StartApplication
 {
+	/*@SpringBootApplication (exclude = { DataSourceAutoConfiguration.class })
+public class SpringbootApplication {
+    SpringApplication.run(SpringbootApplication.class, args);
+}*/
+	//public static final String HOST = "jdbc:postgresql://localhost:5432/";
+	//public static final String DB_NAME = "dvdrental";
+	//public static final String USERNAME = "postgres";
+	//public static final String PASSWORD = "postgres";
 	public static void main(String[] args)
 	{
+		//SpringApplication.run(SpringbootApplication.class, args);
 		SpringApplication.run(StartApplication.class, args);
 	}
 
